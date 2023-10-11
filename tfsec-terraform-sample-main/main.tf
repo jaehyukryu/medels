f113850992030bf83d1de1f81e7bccbf4396b8d9
mendels <59906326+jaehyukryu@users.noreply.github.com>
2023-10-11 15:15:11 +0900

    Delete tfsec-terraform-sample-main directory

--- tfsec-terraform-sample-main/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_elasticache_replication_group" "bad_example" {
-         replication_group_id = "foo"
-         replication_group_description = "my foo cluster"
-         transit_encryption_enabled = true
-         at_rest_encryption_enabled = true
- } 
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkins"
-}
